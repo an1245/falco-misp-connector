9219a95d70336b600cb5805482df25c2aa53910f
andy <127995147+an1245@users.noreply.github.com>
2024-08-03 18:16:35 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 ##############################################
 #   Falco configuration details              #
 ##############################################
-falco_ipv4_rules_file='/etc/falco/rules.d/misp-ipv4-indicators.yaml'
-falco_ipv4_list_name='malicious_ip_list'
-falco_ipv6_rules_file='/etc/falco/rules.d/misp-ipv6-indicators.yaml'
-falco_ipv6_list_name='malicious_ipv6_list'
-falco_cidr_rules_file='/etc/falco/rules.d/misp-cidr-indicators.yaml'
-falco_cidr_list_name='malicious_cidr_list'
+# Outbound Rules
+falco_ipv4_outbound_rules_file='/home/andy/rules/misp-ipv4-outbound-indicators.yaml'
+falco_ipv4_outbound_list_name='malicious_ipv4_outbound_list'
+falco_ipv6_outbound_rules_file='/home/andy/rules/misp-ipv6-outbound-indicators.yaml'
+falco_ipv6_outbound_list_name='malicious_ipv6_outbound_list'
+falco_cidr_outbound_rules_file='/home/andy/rules/misp-cidr-outbound-indicators.yaml'
+falco_cidr_outbound_list_name='malicious_cidr_outbound_list'
+
+# Inbound Rules
+falco_ipv4_inbound_rules_file='/home/andy/rules/misp-ipv4-inbound-indicators.yaml'
+falco_ipv4_inbound_list_name='malicious_ipv4_inbound_list'
+falco_ipv6_inbound_rules_file='/home/andy/rules/misp-ipv6-inbound-indicators.yaml'
+falco_ipv6_inbound_list_name='malicious_ipv6_inbound_list'
+falco_cidr_inbound_rules_file='/home/andy/rules/misp-cidr-inbound-indicators.yaml'
+falco_cidr_inbound_list_name='malicious_cidr_inbound_list'
 
 
 ##############################################
@@ -15,7 +24,6 @@ falco_cidr_list_name='malicious_cidr_list'
 debug = True
 debugindicators = False
 debugyaml = False
-debugtest = False
 
 ##############################################
 #   MISP Connectivity Details                #
